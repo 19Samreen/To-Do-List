3fa91f3a742f147397738f30b1c32761269a87db
Samreen <160302322+19Samreen@users.noreply.github.com>
2024-05-25 09:41:10 +0530

    Add files via upload

---
+++ Project-03.ts
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+
+//To Do List
+
+import inquirer from "inquirer";
+
+let todos : string[] = [];
+let loop = true;
+
+while(loop){
+    const answer : {
+        todo : string,
+        addmore : boolean
+    }
+     = await inquirer.prompt([
+        {
+            type : "input",
+            name : "todo",
+            message : "What do you want to do in your to do list?" 
+        },
+        {
+            type : "confirm",
+            name : "addmore",
+            message : "Do yo want to add more?",
+            default : false 
+        }
+    ])
+    const {todo, addmore} = answer;
+    console.log(answer);
+    loop = addmore
+    if(todo) {
+        todos.push(todo)
+    } else {
+        console.log("Kindly add valid input");
+    }
+}
+
+if(todos.length > 0) {
+    console.log("Your todo List: \n");
+    todos.forEach(todo => {
+        console.log(todo);
+    });
+}else{
+    console.log("No todos found.");
+}
